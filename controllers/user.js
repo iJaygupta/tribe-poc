1232c8f5b70fd474405e7149da43b383c5913d65
iJaygupta <jayguptazzz@gmail.com>
2021-01-07 15:31:56 +0530

    middleware added

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,22 @@
-var jwt = require('jsonwebtoken');
 
 
 
-exports.updateUser = (request, response) => {   
-
+exports.updateUser = (request, response) => {
+    console.log("updateUser triggered");
     console.log("request.body", request.body);
-    console.log("request.query", request.query);
+
+    response.status(200).json({
+        status: "OK",
+        data: {
+            ...request.body
+        }
+    });
+
+}
+
+exports.updateUserPassword = (request, response) => {
+    console.log("updateUserPassword triggered");
+    console.log("request.body", request.body);
 
     response.status(200).json({
         status: "OK",
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 var express = require("express");
 const userController = require("../controllers/user");
+const middleware = require("../lib/auth");
+
 
 
 var router = express.Router();
 
-router.post("/profile", userController.updateUser);
+router.post("/profile", middleware.verifyAccessToken, userController.updateUser);
+router.post("/password", middleware.verifyAccessToken, userController.updateUserPassword);
+
 
 
 module.exports = router;
